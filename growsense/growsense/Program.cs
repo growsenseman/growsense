1eca6c6e9dd4e53c9d86f79dabc77372396fbe74
growsenseman <61911774+growsenseman@users.noreply.github.com>
2020-03-07 19:06:52 +0400

    Update Program.cs

--- growsense/growsense/Program.cs
+++ growsense/growsense/Program.cs
@@ -3,10 +3,8 @@ using System.Windows.Forms;
 
 namespace TrainerBase
 {
-	// Token: 0x02000007 RID: 7
 	internal static class Program
 	{
-		// Token: 0x060000A7 RID: 167 RVA: 0x00018EAA File Offset: 0x000170AA
 		[STAThread]
 		private static void Main()
 		{
