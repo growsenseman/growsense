948ba3145be0604991f30ab57f5781c27818f7ab
growsenseman <61911774+growsenseman@users.noreply.github.com>
2020-03-07 19:28:02 +0400

    Update dikdkdkd.cs

--- growsense/growsense/dikdkdkd.cs
+++ growsense/growsense/dikdkdkd.cs
@@ -7,16 +7,13 @@ using MetroSuite;
 
 namespace TrainerBase
 {
-	// Token: 0x02000002 RID: 2
 	public partial class dikdkdkd : Form
 	{
-		// Token: 0x06000001 RID: 1 RVA: 0x00002048 File Offset: 0x00000248
 		public dikdkdkd()
 		{
 			this.InitializeComponent();
 		}
 
-		// Token: 0x06000002 RID: 2 RVA: 0x00002060 File Offset: 0x00000260
 		private void metroButton16_Click(object sender, EventArgs e)
 		{
 			base.Hide();
@@ -24,7 +21,6 @@ namespace TrainerBase
 			ug.Show();
 		}
 
-		// Token: 0x06000003 RID: 3 RVA: 0x00002084 File Offset: 0x00000284
 		private void dikdkdkd_Load(object sender, EventArgs e)
 		{
 			string fontName = "Century Gothic";
@@ -40,12 +36,10 @@ namespace TrainerBase
 			}
 		}
 
-		// Token: 0x06000004 RID: 4 RVA: 0x000020F0 File Offset: 0x000002F0
 		private void txtPassword_TextChanged(object sender, EventArgs e)
 		{
 		}
 
-		// Token: 0x06000005 RID: 5 RVA: 0x000020F3 File Offset: 0x000002F3
 		private void label2_Click(object sender, EventArgs e)
 		{
 			Environment.Exit(0);
